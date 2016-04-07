cd46e17b5d5b90988f49ef6e3cf2ad6c0f9e04fc
Patrice Jean <gjean.patrice@gmail.com>
2016-04-06 22:11:28 -0300

    Doing some cleanup

--- motor1.py
+++ motor1.py
@@ -1,39 +1,50 @@
 import RPi.GPIO as gpio
 import time
 
-gpio.setmode(gpio.BOARD)
+def init():
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(15, gpio.OUT)
+    gpio.setup(16, gpio.OUT)
+    gpio.setup(18, gpio.OUT)
+    gpio.setup(22, gpio.OUT)
 
-gpio.setup(15, gpio.OUT)
-gpio.setup(16, gpio.OUT)
-gpio.setup(18, gpio.OUT)
-gpio.setup(22, gpio.OUT)
+def forward():
+    init()
+    gpio.output(15, False)
+    gpio.output(16, True)
+    gpio.output(18, True)
+    gpio.output(22, False)
+    time.sleep(1)
+    gpio.cleanup()
+	
+def reverse():
+    init()
+    gpio.output(15, True)
+    gpio.output(16, False)
+    gpio.output(18, False)
+    gpio.output(22, True)
+    time.sleep(1)
+    gpio.cleanup()
 
-'move foward'
-gpio.output(15, True)
-gpio.output(16, False)
-gpio.output(18, False)
-gpio.output(22, True)
-time.sleep(1)
+def turn_right():
+    init()
+    gpio.output(15, False)
+    gpio.output(16, True)
+    gpio.output(18, False)
+    gpio.output(22, True)
+    time.sleep(1)
+    gpio.cleanup()
 
-'move backward'
-gpio.output(15, False)
-gpio.output(16, True)
-gpio.output(18, True)
-gpio.output(22, False)
-time.sleep(1)
+def turn_left():
+    init()
+    gpio.output(15, True)
+    gpio.output(16, False
+    gpio.output(18, True)
+    gpio.output(22, False)
+    time.sleep(3)
+    gpio.cleanup()
 
-'turn right'
-gpio.output(15, False)
-gpio.output(16, True)
-gpio.output(18, False)
-gpio.output(22, True)
-time.sleep(1)
-
-'turn left'
-gpio.output(15, True)
-gpio.output(16, False)
-gpio.output(18, True)
-gpio.output(22, False)
-time.sleep(1)
-
-gpio.cleanup() 
+forward()
+reverse()
+turn_left()
+turn_right()
